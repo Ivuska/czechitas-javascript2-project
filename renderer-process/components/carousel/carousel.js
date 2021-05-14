773916de38f1978b1cb53999e27748f8ecef69c8
Iva Fischerová <iva.fischerova@avast.com>
2021-05-14 19:00:01 +0200

    skoro funkcni domaci ukol

--- renderer-process/components/carousel/carousel.js
+++ renderer-process/components/carousel/carousel.js
@@ -1,3 +1,43 @@
-export class Carousel{
+export class Carousel {
+    constructor() {
+        this.header = document.querySelector('header.header-news > div.header-news__container');
+        this.carouselItemCount = 2;
+        this.carouselItemStart = 0;
+        this.buttonLeft = document.querySelector('#carousel-button-left'); 
+        this.buttonRight = document.querySelector('#carousel-button-right'); 
+        this.buttonLeft.addEventListener('click', () => {
+            this.carouselItemStart --;
+            this.populateNewsCarousel(this.articles);
+        })
+        
+        this.buttonRight.addEventListener('click', () => {
+            this.carouselItemStart ++;
+            this.populateNewsCarousel(this.articles);
+        })
+    }
 
+    populateNewsCarousel(news) {
+        this.articles = news;
+        this.header.innerText = '';
+        for(let i = this.carouselItemStart; i < this.carouselItemStart + this.carouselItemCount; i ++) {
+            const newsValue = news[i];
+            const newsArticle = new NewsArticle(newsValue);
+            this.header.appendChild(newsArticle);
+        }
+        this.checkButtonsVisibility(); 
+      }
+
+    checkButtonsVisibility() {
+        if (this.carouselItemStart === 0) { 
+            this.buttonLeft.hidden = true;
+        }
+        else { 
+            this.buttonLeft.hidden = false;
+        }
+        if (this.carouselItemStart >= (this.articles.length - this.carouselItemCount)) {
+            this.buttonRight.hidden = true;
+        } else {
+            this.buttonRight.hidden = false;
+        }
+    }   
 }
--- renderer-process/main.js
+++ renderer-process/main.js
@@ -1,59 +1,15 @@
 import { NewsArticle } from './components/news-article/news-article.js';
+import { Carousel } from './components/carousel/carousel.js';
 
-const header = document.querySelector('header.header-news > div.header-news__container');
-
-const carouselItemCount = 2;
-let carouselItemStart = 0;
-let articles;
+const carousel = new Carousel;
 
 fetch('http://localhost:3000/news.json')
     .then(serverResponse => serverResponse.text())
     .then(responseText => {
         const data = JSON.parse(responseText);
-        articles = data.articles;
-        populateNewsCarousel(data.articles, 0);
+        carousel.populateNewsCarousel(data.articles);
     });
 
- // domaci ukol c.2   
-function checkButtonsVisibility() {
-    if (carouselItemStart === 0) { 
-        buttonLeft.hidden = true;
-    }
-    else { 
-        buttonLeft.hidden = false;
-    }
-    if (carouselItemStart >= (articles.length - carouselItemCount)) {
-        buttonRight.hidden = true;
-    } else {
-        buttonRight.hidden = false;
-    }
-}
-
-function populateNewsCarousel(news, startAt) {
-    header.innerText = '';
-    for(let i = startAt; i < startAt + carouselItemCount; i ++) {
-        const newsValue = news[i];
-        const newsDiv = new NewsArticle().createDivForNews(newsValue);
-        header.appendChild(newsDiv);
-    }
-    checkButtonsVisibility(); //domaci ukol c.2
-  }
-
-const buttonLeft = document.querySelector('#carousel-button-left')
-
-const buttonRight= document.querySelector('#carousel-button-right')
-
-buttonLeft.addEventListener('click', () => {
-    carouselItemStart --;
-    populateNewsCarousel(articles, carouselItemStart);
-})
-
-buttonRight.addEventListener('click', () => {
-    carouselItemStart ++;
-    populateNewsCarousel(articles, carouselItemStart);
-})
-
-// Domaci ukol cislo 1
 const mainContent = document.querySelector('.main-content')
 
 const daysNumber = 31;
